74e3a87c3801eeebf9f82f8c199da2fdc19102cf
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2021-09-11 21:32:21 +0200

    update playbook and templates to work with latest Ansible and subs-ssl etc

--- templates/django.passenger_wsgi.py
+++ templates/django.passenger_wsgi.py
@@ -1,8 +1,1 @@
-import sys, os
-
-sys.path.append("/home/pacs/{{pac}}/users/{{user}}/mypro")
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mypro.settings")
-
-from django.core.wsgi import get_wsgi_application
-
-application = get_wsgi_application()
+from mypro.wsgi import application
